b947a5ddd17d41479facb8c2ba6bb054e8aca772
ghostlth <1903538331@qq.com>
2017-05-31 22:58:23 +0800

    Update downloadimg.py

--- downloadimg.py
+++ downloadimg.py
@@ -2,9 +2,6 @@ import requests
 import http.cookiejar
 import re,os,time,random
 
-'''
-测试1
-'''
 
 agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36'
 headers = {
